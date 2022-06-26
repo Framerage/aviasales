8dd7a5885a5ae344c3835d3863f78401f901e04b
Artem Igonin <officialigonin@mail.ru>
2022-06-26 18:24:07 +0300

    main

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Content from "./components/Content/Contetn";
 import Header from "./components/Header/Header";
 import './app.css'
 function App() {
+  
   return (
     <div className="wrapper">
       <Header/>
